d195c7280f0e5c6105c1fda29487ad0d360365a1
Fazal Mahmud Niloy <niloooy@gmail.com>
2020-09-08 02:18:09 +0600

    initial commit :tada:

---
+++ src/Pairs.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+
+public class Pairs {
+
+    public static void main(String[] args) {
+        System.out.println("Exercise 1 output produced by Munira Tabassum");
+        System.out.println();
+        int[] arr = {8,7,2,34,3,9,14,4,99,99,1,2,3,4,56,5,6};
+        PrintPairs(arr);
+    }
+
+    private static void PrintPairs(int[] arr) {
+        Arrays.sort(arr);
+        int previous = 0;
+        boolean leftNew = false;
+
+        System.out.println("Current Array");
+        System.out.println(Arrays.toString(arr));
+        System.out.println();
+
+        for(int i = 0; i < arr.length-1; i++) {
+            leftNew = true;
+            for(int j = i+1; j < arr.length; j++) {
+                if((leftNew || arr[j] != previous) && (arr[i] != arr[j])) {
+                    System.out.println(arr[i] + "," + arr[j]);
+                    previous = arr[j];
+                    leftNew = false;
+                }
+            }
+        }
+    }
+}
